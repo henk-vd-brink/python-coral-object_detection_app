36e8315480937c28ed2736b8346daba74fd9a8d6
Henk van den Brink <hcebrink@gmail.com>
2022-01-27 14:04:11 +0100

    wip

--- app/detectors/dice_detector.py
+++ app/detectors/dice_detector.py
@@ -136,7 +136,7 @@ class DiceDetector(BaseDetector):
         img = img_dict["empty_sum"].copy()
 
         font = cv2.FONT_HERSHEY_DUPLEX
-        bottomLeftCornerOfText = (0, 49)
+        bottomLeftCornerOfText = (5, 44)
         fontScale = 1
         fontColor = (254, 254, 254)
         thickness = 2
